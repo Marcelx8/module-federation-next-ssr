5a49713e1cf172f07cd1d93c11f312f2d93df56b
Marcelx8 <marcelsteenkamp3@gmail.com>
2022-02-02 09:54:16 +0200

    changed module imports to be top-level await imports

---
+++ pdp/pages/_app.tsx
@@ -0,0 +1,28 @@
+import type { AppProps } from 'next/app'
+// import Script from 'next/script'
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      {/* <Script
+        data-webpack="server"
+        src="http://localhost:3000/_next/static/chunks/remoteEntry.js"
+      />
+      <Script
+        data-webpack="home"
+        src="http://localhost:3001/_next/static/chunks/remoteEntry.js"
+      />
+      <Script
+        data-webpack="products"
+        src="http://localhost:3002/_next/static/chunks/remoteEntry.js"
+      />
+      <Script
+        data-webpack="ui"
+        src="http://localhost:3003/_next/static/chunks/remoteEntry.js"
+      /> */}
+
+        <Component {...pageProps} />
+    </>
+  )
+}
+
+export default MyApp
