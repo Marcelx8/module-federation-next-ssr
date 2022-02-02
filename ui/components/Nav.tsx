48a0973633f41f1f37675a0f46ca8bbdb92a286a
Marcelx8 <marcelsteenkamp3@gmail.com>
2022-02-02 09:56:27 +0200

    made nav look better

--- ui/components/Nav.tsx
+++ ui/components/Nav.tsx
@@ -14,40 +14,10 @@ function Nav() {
       <ul>
         {links.map(({ href, label }) => (
           <Link key={`nav-link-${label}`} href={href}>
-            <a>{label}</a>
+            <a>- {label} -</a>
           </Link>
         ))}
       </ul>
-
-      {/* <style jsx>{`
-      :global(body) {
-        margin: 0;
-        font-family: -apple-system, BlinkMacSystemFont, Avenir Next, Avenir,
-          Helvetica, sans-serif;
-      }
-      nav {
-        text-align: center;
-      }
-      ul {
-        display: flex;
-        justify-content: space-between;
-      }
-      nav > ul {
-        padding: 4px 16px;
-        max-width: 600px;
-        margin: 0 auto;
-      }
-      li {
-        display: flex;
-        padding: 6px 8px;
-      }
-      a {
-        color: black;
-        text-decoration: none;
-        font-size: 13px;
-        padding-right: 10px;
-      }
-    `}</style> */}
     </nav>
   )
 }
